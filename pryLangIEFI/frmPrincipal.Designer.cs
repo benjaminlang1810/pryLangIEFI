95e7c4c77776e1a48c1e6ad1e2999237c6b89646
benja lang <benjalang2016@gmail.com>
2023-07-01 20:33:32 -0300

    Agregar archivos de proyecto.

---
+++ pryLangIEFI/frmPrincipal.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace pryLangIEFI
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnListado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.Location = new System.Drawing.Point(39, 9);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(432, 89);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido";
+            this.lblBienvenido.Click += new System.EventHandler(this.lblBienvenido_Click);
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Location = new System.Drawing.Point(2, 101);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(163, 68);
+            this.btnProductos.TabIndex = 1;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Location = new System.Drawing.Point(171, 101);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(163, 68);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnListado
+            // 
+            this.btnListado.Location = new System.Drawing.Point(340, 101);
+            this.btnListado.Name = "btnListado";
+            this.btnListado.Size = new System.Drawing.Size(163, 68);
+            this.btnListado.TabIndex = 3;
+            this.btnListado.Text = "Listado";
+            this.btnListado.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(521, 200);
+            this.Controls.Add(this.btnListado);
+            this.Controls.Add(this.btnVentas);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.lblBienvenido);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnListado;
+    }
+}
---
+++ pryLangIEFI/frmVentas.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace pryLangIEFI
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVentas = new System.Windows.Forms.Label();
+            this.gbNuevaVenta = new System.Windows.Forms.GroupBox();
+            this.txtProductos = new System.Windows.Forms.TextBox();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblFechaVenta = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.gbNuevaVenta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblVentas
+            // 
+            this.lblVentas.AutoSize = true;
+            this.lblVentas.Font = new System.Drawing.Font("Arial", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVentas.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblVentas.Location = new System.Drawing.Point(60, 9);
+            this.lblVentas.Name = "lblVentas";
+            this.lblVentas.Size = new System.Drawing.Size(155, 50);
+            this.lblVentas.TabIndex = 0;
+            this.lblVentas.Text = "Ventas";
+            // 
+            // gbNuevaVenta
+            // 
+            this.gbNuevaVenta.Controls.Add(this.btnVolver);
+            this.gbNuevaVenta.Controls.Add(this.btnCargar);
+            this.gbNuevaVenta.Controls.Add(this.textBox1);
+            this.gbNuevaVenta.Controls.Add(this.lblCantidad);
+            this.gbNuevaVenta.Controls.Add(this.dateTimePicker1);
+            this.gbNuevaVenta.Controls.Add(this.lblFechaVenta);
+            this.gbNuevaVenta.Controls.Add(this.lblProducto);
+            this.gbNuevaVenta.Controls.Add(this.txtProductos);
+            this.gbNuevaVenta.Location = new System.Drawing.Point(8, 74);
+            this.gbNuevaVenta.Name = "gbNuevaVenta";
+            this.gbNuevaVenta.Size = new System.Drawing.Size(301, 358);
+            this.gbNuevaVenta.TabIndex = 1;
+            this.gbNuevaVenta.TabStop = false;
+            this.gbNuevaVenta.Text = "Nueva Venta";
+            // 
+            // txtProductos
+            // 
+            this.txtProductos.Location = new System.Drawing.Point(23, 49);
+            this.txtProductos.Name = "txtProductos";
+            this.txtProductos.Size = new System.Drawing.Size(211, 22);
+            this.txtProductos.TabIndex = 0;
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProducto.Location = new System.Drawing.Point(23, 26);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(81, 20);
+            this.lblProducto.TabIndex = 1;
+            this.lblProducto.Text = "Producto:";
+            // 
+            // lblFechaVenta
+            // 
+            this.lblFechaVenta.AutoSize = true;
+            this.lblFechaVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaVenta.Location = new System.Drawing.Point(23, 87);
+            this.lblFechaVenta.Name = "lblFechaVenta";
+            this.lblFechaVenta.Size = new System.Drawing.Size(108, 20);
+            this.lblFechaVenta.TabIndex = 2;
+            this.lblFechaVenta.Text = "Fecha Venta:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(23, 110);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(211, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(19, 158);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(80, 20);
+            this.lblCantidad.TabIndex = 4;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(150, 220);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(135, 40);
+            this.btnCargar.TabIndex = 6;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(9, 220);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(135, 40);
+            this.btnVolver.TabIndex = 7;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // frmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(404, 450);
+            this.Controls.Add(this.gbNuevaVenta);
+            this.Controls.Add(this.lblVentas);
+            this.Name = "frmVentas";
+            this.Text = "frmVentas";
+            this.gbNuevaVenta.ResumeLayout(false);
+            this.gbNuevaVenta.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVentas;
+        private System.Windows.Forms.GroupBox gbNuevaVenta;
+        private System.Windows.Forms.Label lblFechaVenta;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.TextBox txtProductos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
